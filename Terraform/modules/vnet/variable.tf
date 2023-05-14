444680afe6555ca3c3b87533e41d2135fd551432
sp407 <131864941+sp407@users.noreply.github.com>
2023-05-14 11:11:11 +0530

    Update variable.tf

--- Terraform/modules/vnet/variable.tf
+++ Terraform/modules/vnet/variable.tf
@@ -1,3 +1,15 @@
 variable "vnet_name" {
   type = string
 }
+
+variable "vnet_address_space" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
