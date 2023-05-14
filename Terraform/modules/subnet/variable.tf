2bcb208eb4e981f2504b64bf2e8814021e0a1981
sp407 <131864941+sp407@users.noreply.github.com>
2023-05-14 15:49:27 +0530

    Create variable.tf

---
+++ Terraform/modules/subnet/variable.tf
@@ -0,0 +1,15 @@
+variable "vm_subnet" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "subnet_address_prefixes" {
+  type = string
+}
