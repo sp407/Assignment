bf78763696c5b5c558eef80b5cd1525a08f09670
sp407 <131864941+sp407@users.noreply.github.com>
2023-05-14 10:30:05 +0530

    Update main.tf

--- Terraform/modules/vm/main.tf
+++ Terraform/modules/vm/main.tf
@@ -1,1 +1,27 @@
+resource "azurerm_linux_virtual_machine" "VM_Redhat" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.size
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.myvmnic.id
+  ]
 
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = var.caching
+    storage_account_type = var.storage_account_type
+  }
+
+  source_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.version
+  }
+}
