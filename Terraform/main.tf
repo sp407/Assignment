f931dc1f62c8bf3491cf86841bc3591c352febf8
sp407 <131864941+sp407@users.noreply.github.com>
2023-05-14 16:40:09 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,3 +2,23 @@ provider "azurerm" {
     version = "=2.0.0"
     features {}
 }
+
+data "azurerm_resource_group" "rg"{
+    name = var.rg_name
+}
+
+module "virtual-network"{
+    source = "./modules/vnet"
+    vnet_name                       = var.vnet_name
+    resource_group_name             = var.resource_group_name
+    location                        = var.location
+    vnet_address_space              = var.vnet_address_space
+}
+    
+ module "subnet"{
+    source = "./modules/subnet"
+    vm_subnet_name                  = var.vm_subnet_name
+    resource_group_name             = var.resource_group_name
+    vnet_name                       = var.vnet_name
+    subnet_address_prefixes         = var.subnet_address_prefixes
+ }
