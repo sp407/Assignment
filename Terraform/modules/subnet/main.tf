148cc506ec7af1eafcfc8c9d486a3e28dfab9b37
sp407 <131864941+sp407@users.noreply.github.com>
2023-05-14 17:25:43 +0530

    Update main.tf

--- Terraform/modules/subnet/main.tf
+++ Terraform/modules/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "subnet" {
-  name                 = var.vm_subnet
+  name                 = var.vm_subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = var.subnet_address_prefixes
