9620789b335037067bcb930300113a27c4815486
sp407 <131864941+sp407@users.noreply.github.com>
2023-05-14 15:54:03 +0530

    Create main.tf

---
+++ Terraform/modules/network-interface/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_network_interface" "vm_nic" {
+    name                = var.vm_nic_name
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    ip_configuration {
+        name                          = "internal"
+        subnet_id                     = var.subnet_id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
