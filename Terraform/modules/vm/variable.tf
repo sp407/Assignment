f8a9716f9bea03c2287db5520bab8a439ab723ed
sp407 <131864941+sp407@users.noreply.github.com>
2023-05-14 10:34:40 +0530

    Create variable.tf

---
+++ Terraform/modules/vm/variable.tf
@@ -0,0 +1,43 @@
+variable "vm_name" {
+  type = string
+}
+
+variable "resource_group_name"
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "size" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "caching" {
+  type = string
+}
+
+variable "storage_account_type" {
+  type = string
+}
+
+variable "publisher" {
+  type = string
+}
+
+variable "offer" {
+  type = string
+}
+
+variable "sku" {
+  type = string
+}
+
+variable "version" {
+  type = string
+}
